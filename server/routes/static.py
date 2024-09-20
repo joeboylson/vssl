542e79fe114077f7ca773448a6bd297355ac08b3
Joe Boylson <joeboylson@gmail.com>
2024-09-20 15:50:13 -0400

    expempt idex route from limiter;

--- server/routes/static.py
+++ server/routes/static.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, send_file, redirect
 from utils.environment import is_production
+from utils.limiter import app_limiter
 
 STATIC_FOLDER_NAME = "dist"
 static_blueprint = Blueprint("static_blueprint", __name__)
 
 
 @static_blueprint.route("/")
+@app_limiter.exempt
 def index():
     if is_production():
         return send_file(f"{STATIC_FOLDER_NAME}/index.html")
